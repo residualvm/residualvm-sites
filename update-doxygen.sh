f0c0bca22f659e5db5bcf017a25c55ba09023241
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2014-09-03 05:55:40 +0200

    initial import doxygen files

---
+++ update-doxygen.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+BASEPATH=/home/doxygen
+
+source ${BASEPATH}/lock_unlock
+
+LOCKFILE=${BASEPATH}/doxygen.lock
+
+# Create a lockfile (and delete it if we abort)
+lock_unlock action=lock name=$LOCKFILE
+
+echo "Updating GIT..."
+cd ${BASEPATH}/sources
+nice git pull
+
+echo Updating doxygen docs
+cd ${BASEPATH}
+nice doxygen doxygen.conf
+
+# Fix the index.html file <title> if necessary
+perl -pi -e 's,<title>ResidualVM</title>,<title>ResidualVM :: Doxygen</title>,g' /var/www/doxygen/html/index.html
+
+cp -R images /var/www/doxygen/html/
+cp residualvm_tabs.css /var/www/doxygen/html/
+cp favicon.ico /var/www/doxygen/html/
+
+lock_unlock action=unlock name=$LOCKFILE
